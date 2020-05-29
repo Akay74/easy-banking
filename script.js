498bb935a4f06e839d6f1ee620ae23b8aedc7946
Akachukwu Udeh <57964896+Akay74@users.noreply.github.com>
2020-05-29 23:12:46 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("it works!");
+
+let closeButton = document.querySelector(".close-icon");
+    closeButton.addEventListener( "click", hideNavigationLinks );
+
+
+let hamburgerButton = document.querySelector(".hamburger-icon");
+    hamburgerButton.addEventListener( "click", showNavigationLinks );
+
+let navigationLinks = document.querySelector(".mobile-nav");
+
+
+function showNavigationLinks() {
+        console.log("Hamburger button clicked");
+        hamburgerButton.classList.toggle("hidden");
+        closeButton.classList.toggle("hidden");
+        navigationLinks.classList.toggle("hidden");
+}
+
+function hideNavigationLinks() {
+        console.log("Close button clicked");
+        hamburgerButton.classList.toggle("hidden");
+        closeButton.classList.toggle("hidden");
+        navigationLinks.classList.toggle("hidden");
+}
