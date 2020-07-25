c728be490aa37c539845f4bd659780f681c7795f
Akachukwu Udeh <akaudeh@gmail.com>
2020-07-25 19:41:51 +0100

    Changes to the html, css and js files

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
-console.log("it works!");
+console.log('test. it works');
 
-let closeButton = document.querySelector(".close-icon");
-    closeButton.addEventListener( "click", hideNavigationLinks );
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
 
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("active");
+});
 
-let hamburgerButton = document.querySelector(".hamburger-icon");
-    hamburgerButton.addEventListener( "click", showNavigationLinks );
-
-let navigationLinks = document.querySelector(".mobile-nav");
-
-function showNavigationLinks() {
-        console.log("Hamburger button clicked");
-        hamburgerButton.classList.toggle("hidden");
-        closeButton.classList.toggle("hidden");
-        navigationLinks.classList.toggle("hidden");
-}
-
-function hideNavigationLinks() {
-        console.log("Close button clicked");
-        hamburgerButton.classList.toggle("hidden");
-        closeButton.classList.toggle("hidden");
-        navigationLinks.classList.toggle("hidden");
-}
+$(window).scroll(() => {
+    $('nav').toggleClass("scrolled", $(this).scrollTop() > 60);
+}); 
